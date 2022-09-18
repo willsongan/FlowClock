9faf2ff6b52fb8c46abd9596d0c7a204558b1e0f
Willson Gan <willsongan30@gmail.com>
2022-09-19 03:36:10 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace FlowClock
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.headerTablePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.contentTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.headerTablePanel.SuspendLayout();
+            this.contentTableLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.closeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Location = new System.Drawing.Point(141, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(41, 35);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "X";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(21, 30);
+            this.timeLabel.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(143, 34);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "00:00:00";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 99);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(179, 41);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(62, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zzz";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // headerTablePanel
+            // 
+            this.headerTablePanel.ColumnCount = 3;
+            this.headerTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.headerTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.headerTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.headerTablePanel.Controls.Add(this.closeButton, 2, 0);
+            this.headerTablePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerTablePanel.Location = new System.Drawing.Point(0, 0);
+            this.headerTablePanel.Name = "headerTablePanel";
+            this.headerTablePanel.RowCount = 1;
+            this.headerTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.headerTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.headerTablePanel.Size = new System.Drawing.Size(185, 50);
+            this.headerTablePanel.TabIndex = 3;
+            this.headerTablePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tableLayoutPanel2_MouseDown);
+            // 
+            // contentTableLayout
+            // 
+            this.contentTableLayout.ColumnCount = 1;
+            this.contentTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.contentTableLayout.Controls.Add(this.timeLabel, 0, 0);
+            this.contentTableLayout.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.contentTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentTableLayout.Location = new System.Drawing.Point(0, 50);
+            this.contentTableLayout.Name = "contentTableLayout";
+            this.contentTableLayout.RowCount = 2;
+            this.contentTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.contentTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.contentTableLayout.Size = new System.Drawing.Size(185, 145);
+            this.contentTableLayout.TabIndex = 4;
+            this.contentTableLayout.DoubleClick += new System.EventHandler(this.contentTableLayout_DoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(121, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(185, 195);
+            this.Controls.Add(this.contentTableLayout);
+            this.Controls.Add(this.headerTablePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(185, 195);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.headerTablePanel.ResumeLayout(false);
+            this.contentTableLayout.ResumeLayout(false);
+            this.contentTableLayout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel headerTablePanel;
+        private System.Windows.Forms.TableLayoutPanel contentTableLayout;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace FlowClock
+{
+    public partial class mainForm : Form
+    {
+        #region DraggableWindow
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        #endregion
+
+        //timer
+        System.Timers.Timer timer;
+        Action timerBehavior;
+        int milisecond, second, minute;
+        public bool isPlaying;
+        public bool isResting;
+
+        //timeLabelFont
+        public float minTimeLabelFont = 21.75f;
+        public float maxTimeLabelFont = 38.25f;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            contentTableLayout.MouseWheel += ContentTableLayout_MouseWheel;
+        }
+
+        private void ContentTableLayout_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if(e.Delta > 0)
+            {
+                Size += new Size(6,5);
+                var fontSizeAfter = timeLabel.Font.Size + 0.8f;
+                if (fontSizeAfter > maxTimeLabelFont) fontSizeAfter = maxTimeLabelFont;
+                timeLabel.Font = new Font(timeLabel.Font.FontFamily,fontSizeAfter);
+            }
+            else
+            {
+                Size -= new Size(5,6);
+                var fontSizeBefore = timeLabel.Font.Size - 1;
+                if (fontSizeBefore < minTimeLabelFont) fontSizeBefore = minTimeLabelFont;
+                timeLabel.Font = new Font(timeLabel.Font.FontFamily, fontSizeBefore);
+            }
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            //mainForm
+            TopMost = true;
+            Size = MinimumSize;
+
+            //closeButton
+            closeButton.TabStop = false;
+            closeButton.FlatStyle = FlatStyle.Flat;
+            closeButton.FlatAppearance.BorderSize = 0;
+            closeButton.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+
+            //timer
+            timer = new System.Timers.Timer();
+            timer.Interval = 10;
+            timerBehavior = CountUpTimer;
+            timer.Elapsed += OnTimeEvent;
+            button2.Enabled = false;
+
+            //button
+            button1.MouseUp += Button1_MouseUp;
+            button2.MouseUp += Rest;
+            button3.MouseUp += Button3_MouseUp;
+        }
+
+        private void Button3_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer.Stop();
+            SetTimerZero();
+            Print();
+            isPlaying = false;
+            button1.Text = ">";
+            button2.Enabled = false;
+        }
+
+        private void Button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //initial start, isPlaying jadi pause/stop
+            if (!isPlaying)
+            {
+                timerBehavior = CountUpTimer;
+                timer.Start();
+                isPlaying = true;
+                button1.Text = "||";
+            }
+            else
+            {
+                timer.Stop();
+                isPlaying = false;
+                button1.Text = ">";
+            }
+        }
+
+        private void Rest(object sender, MouseEventArgs e)
+        {
+            isPlaying = false;
+            isResting = true;
+
+            minute = 5;
+            second = 0;
+            milisecond = 0;
+
+            timerBehavior = CountDownTimer;
+            timer.Start();
+
+            button2.MouseUp -= Rest;
+            button2.MouseUp += IncreaseRestTime;
+
+            button3.Enabled = false;
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(timerBehavior);
+            Invoke(new Action(SetRestButtonState));
+        }
+
+        private void SetRestButtonState()
+        {
+            if (isPlaying)
+            {
+                bool passFlowTime = minute > 14;
+                button2.Enabled = passFlowTime;
+            }
+            
+            if (isResting)
+            {
+                button2.Text = "m+=1";
+            }
+        }
+
+        private void IncreaseRestTime(object sender, MouseEventArgs e)
+        {
+            minute += 1;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            //is this needed? just to be safe first
+            Dispose();
+
+            Close();
+        }
+
+        private void tableLayoutPanel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+
+        private void CountUpTimer()
+        {
+            Print();
+            CountUp();
+
+            void CountUp()
+            {
+                milisecond += 1;
+                if (milisecond > 99)
+                {
+                    second += 1;
+                    milisecond = 0;
+                }
+
+                if (second == 60)
+                {
+                    minute += 1;
+                    second = 0;
+                }
+            }
+        }
+
+        bool isMinSize = false;
+        private void contentTableLayout_DoubleClick(object sender, EventArgs e)
+        {
+            if(isMinSize)
+            {
+                Size = MaximumSize;
+                timeLabel.Font = new Font(timeLabel.Font.FontFamily, maxTimeLabelFont);
+                isMinSize = false;
+            }
+            else
+            {
+                Size = MinimumSize;
+                timeLabel.Font = new Font(timeLabel.Font.FontFamily, minTimeLabelFont);
+                isMinSize = true;
+            }
+            
+        }
+
+        private void Print(bool showMiliSec = true)
+        {
+            if(showMiliSec)
+            {
+                var m = minute.ToString().PadLeft(2, '0');
+                var s = second.ToString().PadLeft(2, '0');
+                var ms = milisecond.ToString().PadLeft(2, '0');
+                timeLabel.Text = string.Format("{0}:{1}:{2}", m, s, ms);
+            }
+            else
+            {
+                var m = minute.ToString().PadLeft(2, '0');
+                var s = second.ToString().PadLeft(2, '0');
+                timeLabel.Text = string.Format("{0}:{1}", m, s);
+            }
+            
+        }
+
+        private void CountDownTimer()
+        {
+            button1.Enabled = false;
+            CountDown();
+            Print(showMiliSec:false);
+
+            void CountDown()
+            {
+                milisecond -= 1;
+
+                if (milisecond > 0) return;
+                milisecond += 99;
+                second -= 1;
+                if (second > 0) return;
+                second += 60;
+                minute -= 1;
+                if (minute >= 0) return;
+                RestFinished();
+            }
+        }
+
+        private void SetTimerZero()
+        {
+            minute = 0;
+            second = 0;
+            milisecond = 0;
+        }
+
+        private void RestFinished()
+        {
+            timer.Stop();
+            SetTimerZero();
+            isResting = false;
+            button2.Text = "Zzz";
+            button2.MouseUp -= IncreaseRestTime;
+            button2.MouseUp += Rest;
+            button2.Enabled = false;
+            button1.Text = ">";
+            button1.Enabled = true;
+            button3.Enabled = true;
+        }
+    }
+}
